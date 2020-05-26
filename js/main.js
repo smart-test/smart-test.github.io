8a922ce99f3402a2c3ed0b0d25dbe2931eef9862
smart-test <65768263+smart-test@users.noreply.github.com>
2020-05-27 00:15:52 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -91,6 +91,100 @@ $(document).ready(function(){
   $('#capSelect .capItem').on('click', function(){
     var imgPath;
     imgPath = $(this).attr('data-img-path');
-    $('#imgHolder img').attr('src', imgPath);
+    $('#imgHolder img').fadeOut(500, function(){
+      $('#imgHolder img').attr('src', imgPath).fadeIn(300);
+    });
   });
+});
+
+$(document).ready(function(){
+  $('#capSelect1 .capItem').on('click', function(){
+    var imgPath;
+    imgPath = $(this).attr('data-img-path');
+    $('#imgHolder1 img').fadeOut(500, function(){
+      $('#imgHolder1 img').attr('src', imgPath).fadeIn(300);
+    });
+    
+  });
+});
+
+$(".opacity").click(function(e) {
+  e.preventDefault();
+  $(".opacity").removeClass('active');
+  $(this).addClass('active');
+})
+$(".opacity1").click(function(e) {
+  e.preventDefault();
+  $(".opacity1").removeClass('active');
+  $(this).addClass('active');
+})
+$('.capItem').hover(
+  function () {
+    $(this).addClass('active1');
+  },
+  function () {
+    $(this).removeClass('active1');
+  }
+);
+
+
+/* Скролл вверх */
+$(document).ready(function(){
+
+  $(function (){
+  $("#back-top").hide();
+
+  $(window).scroll(function (){
+    if ($(this).scrollTop() > 700){
+      $("#back-top").fadeIn();
+    } else{
+      $("#back-top").fadeOut();
+    }
+  });
+  $("#back-top").click(function (){
+    $("body,html").animate({
+      scrollTop:0
+    }, 800);
+    return false;
+  });
+});
+});
+$(document).ready(function() { // Ждём загрузки страницы
+	
+	$(".popup-start").click(function(){	// Событие клика на маленькое изображение
+	  	var img = $(this);	// Получаем изображение, на которое кликнули
+		var src = img.attr('src'); // Достаем из этого изображения путь до картинки
+		$("body").append("<div class='popup'>"+ //Добавляем в тело документа разметку всплывающего окна
+						 "<div class='popup_bg'></div>"+ // Блок, который будет служить фоном затемненным
+						 "<img src='"+src+"' class='popup_img' />"+ // Само увеличенное фото
+						 "</div>"); 
+		$(".popup").fadeIn(800); // Медленно выводим изображение
+		$(".popup_bg").click(function(){	// Событие клика на затемненный фон	   
+			$(".popup").fadeOut(800);	// Медленно убираем всплывающее окно
+			setTimeout(function() {	// Выставляем таймер
+			  $(".popup").remove(); // Удаляем разметку всплывающего окна
+			}, 800);
+		});
+	});
+	
+});
+
+$(document).ready(function() { 
+	
+	$(".popup-start-big").click(function(){
+	  	var img = $(this);
+		var src = img.attr('src'); 
+		$("body").append("<div class='popup'>"+ 
+						 "<div class='popup_bg'></div>"+ 
+						 "<img src='"+src+"' class='popup_img-big' />"+ 
+						 "</div>"); 
+		$(".popup").fadeIn(800); 
+		$(".popup_bg").click(function(){	  
+			$(".popup").fadeOut(800);	
+			setTimeout(function() {	
+			  $(".popup").remove(); 
+			}, 800);
+		});
+	});
+	
 });
